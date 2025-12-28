04b23dbffe5883fa3b811a27f914974ada9688b8
Rohan Jayawant Dalvi <87616784+rohan-dalvi1402@users.noreply.github.com>
2025-12-27 19:49:28 -0500

    Update install-service.sh

--- service/install-service.sh
+++ service/install-service.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+sudo cp processor.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable processor
+sudo systemctl start processor
+
+echo "[+] Service installed and running."
 
